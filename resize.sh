fbaa59faae8dfe9f2c01491ab5a6767c844a8402
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2020-03-11 16:53:05 -0400

    fix folder name

--- resize.sh
+++ resize.sh
@@ -4,13 +4,13 @@
 
 
 ## Resize all the jpg's to be 32x32
-find ../datasets_tmp -regex ".*\(jpg\)" -exec echo {} \; -execdir convert {} -resize 32x32 {} \;
+find ../datasets -regex ".*\(jpg\)" -exec echo {} \; -execdir convert {} -resize 32x32 {} \;
 
 echo "Done with resizing\n"
 echo "Checking sizes\n"
 
 ## check
-find ../datasets_tmp/ -regex ".*\(jpg\)" -exec identify {} \; | awk '{print $1,$3}' > ../check_image_sizes.dat
+find ../datasets -regex ".*\(jpg\)" -exec identify {} \; | awk '{print $1,$3}' > ../check_image_sizes.dat
 
-find ../datasets_tmp/ -regex ".*\(jpg\)" -print0 | xargs -P 12 -0 identify | awk '{print $1,$3}' | sed "s/ /,/" > ../check_image_sizes.csv
+find ../datasets -regex ".*\(jpg\)" -print0 | xargs -P 12 -0 identify | awk '{print $1,$3}' | sed "s/ /,/" > ../check_image_sizes.csv
 

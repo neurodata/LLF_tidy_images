f8462e4b75c021a50642ea502c1b8560564ec015
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2020-03-13 10:37:40 -0400

    change folder

--- resize.sh
+++ resize.sh
@@ -3,10 +3,10 @@
 ## ImageMagick is required along with tensorflow
 
 ## Resize all the jpg's to be 32x32
-find ../datasets -type f -regex ".*\(jpg\)" | xargs -P 12 -L 1 ./resize.py
+find ../datasets_resized -type f -regex ".*\(jpg\)" | xargs -P 12 -L 1 ./resize.py
 
 echo "Done with resizing.\n"
 echo "Checking sizes...\n"
 
 ## check
-find ../datasets_tmp -type f -regex ".*\(jpg\)" -exec identify {} \; | awk '{print $1,$3}' > ../check_image_sizes.dat
+find ../datasets_resized -type f -regex ".*\(jpg\)" -exec identify {} \; | awk '{print $1,$3}' > ../check_image_sizes.dat

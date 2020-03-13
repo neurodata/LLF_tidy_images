73a738bfd89f61377b6150c5561e0cc511ef5ea1
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2020-03-13 00:00:12 -0400

    add convert step

--- cropBanner.sh
+++ cropBanner.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-find ../datasets/fgvc-aircraft-2013b -type f -regex ".*\(jpg\)" -execdir convert {} -gravity South -crop +0+20 +repage {} \;
+find ../datasets_cropped/fgvc-aircraft-2013b -type f -regex ".*\(jpg\)" -execdir convert {} -gravity South -crop +0+20 +repage {} \;

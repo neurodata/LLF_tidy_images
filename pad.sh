56f952318bcc6ea44e3093ee7dac6350563c10ad
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2020-03-13 00:40:44 -0400

    change folder

--- pad.sh
+++ pad.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-find ../datasets -type f -regex ".*\(jpg\)" | xargs -P 12 -L 1 ./padding.py
+find ../datasets_padded -type f -regex ".*\(jpg\)" | xargs -P 12 -L 1 ./padding.py

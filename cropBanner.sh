81ab1cbbf70a561a4f78e6919c52194bc7f49ade
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2020-03-30 12:09:31 -0400

    fix currupt birdsnap image.

--- cropBanner.sh
+++ cropBanner.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 find ../datasets_cropped/fgvc-aircraft-2013b -type f -regex ".*\(jpg\)" -execdir convert {} -gravity South -crop +0+20 +repage {} \;
+
+convert ../datasets_cropped/birdsnap/download/images/Mourning_Dove/181010.jpg -gravity South -crop +0+20 +repage ../datasets_cropped/birdsnap/download/images/Mourning_Dove/181010.jpg

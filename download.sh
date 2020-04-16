4355627981cf97e2b8c86f13bb3d94f552b19271
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2020-04-16 15:07:27 -0400

    add flower dict. and update download script.

--- download.sh
+++ download.sh
@@ -1,37 +1,96 @@
 #!/usr/env bash
 
+WORKDIR="$PWD"
 if false; then
 # Setup
-	WRKDIR="$PWD"
-	mkdir ../compressed
+	#mkdir ../compressed
 	mkdir ../datasets_ORIGINAL
 	mkdir ../datasets_cropped
 	mkdir ../datasets_padded
 	mkdir ../datasets_resized
 	mkdir ../datasets_resized_wLabels
 
+fi
 
 #Download
-	cd ../compressed
+	cd ../datasets_ORIGINAL
+	OG="$PWD"
 
-## 101_ObjectCategories CalTech
-	wget http://www.vision.caltech.edu/Image_Datasets/Caltech101/101_ObjectCategories.tar.gz
 
 ## Cars: http://host.robots.ox.ac.uk/pascal/VOC/voc2007/
+
+  if false; then
+## 101_ObjectCategories CalTech
+  cd $OG
+  wget http://www.vision.caltech.edu/Image_Datasets/Caltech101/101_ObjectCategories.tar.gz
+
+  tar -xzf 101_ObjectCategories.tar.gz
+
+  cd $OG
+	mkdir Cars; cd Cars; CARS="$PWD"
 	# train/val data
-	wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar
+  wget http://imagenet.stanford.edu/internal/car196/cars_train.tgz
+  # test data
+  wget http://imagenet.stanford.edu/internal/car196/cars_test.tgz
+
+	tar -xzf cars_train.tgz
+	tar -xzf cars_test.tgz
 	
 	# devkit with training labels
 	wget http://ai.stanford.edu/~jkrause/cars/car_devkit.tgz
-	#tar -xzf car_devkit.tgz
+	tar -xzf car_devkit.tgz
 
 	# test labels
 	wget http://imagenet.stanford.edu/internal/car196/cars_test_annos_withlabels.mat
 
+  
 ## 102-Flowers
+  cd $OG
 	mkdir 102Flowers; cd 102Flowers
+  FLOWERS=$PWD
 	wget https://www.robots.ox.ac.uk/~vgg/data/flowers/102/102flowers.tgz
 	wget https://www.robots.ox.ac.uk/~vgg/data/flowers/102/imagelabels.mat
+  wget http://www.robots.ox.ac.uk/~vgg/data/flowers/102/setid.mat
+  cp $WORKDIR/102Flower_labels.csv $FLOWERS/
 
-fi
+  tar -xzf 102flowers.tgz
+
+## SUN397
+  cd $OG
+	mkdir SUN397; cd SUN397;
+	wget http://vision.princeton.edu/projects/2010/SUN/SUN397.tar.gz
+	wget https://vision.princeton.edu/projects/2010/SUN/download/Partitions.zip 
+
+  tar -xzf SUN397.tar.gz
+  mkdir Partitions
+  unzip Partitions.zip -d Partitions
+
+## FGVC (Aircraft)
+  cd $OG
+  mkdir FGVC; cd FGCV;
+  wget https://www.robots.ox.ac.uk/~vgg/data/fgvc-aircraft/archives/fgvc-aircraft-2013b.tar.gz
+
+  tar -xzf fgvc-aircraft-2013b.tar.gz
+
+  fi 
+
+
+
+
+
+
+## CLEANING
+#if false; then
+#  #  find $OG -regex ".*\(tgz\|tar.gz\)" -exec mv {} ../compressed/ ;
+#fi
+
+
+
+
+
+
+
+
+
+
 
--- tidyLabels.py
+++ tidyLabels.py
@@ -6,6 +6,7 @@ from scipy.io import loadmat
 import csv
 import glob
 import os
+import numpy as np
 import argparse
 import configparser
 import pandas as pd
@@ -21,7 +22,7 @@ def Object101(directory):
     folders = os.listdir(ob101)
     classes = [i.lower() for i in folders]
 
-    dOBJ101 = {'filePath' : [], 'baseName' : [], 'label0' : []}
+    dOBJ101 = {'dataset' : [], 'filePath' : [], 'baseName' : [], 'label0' : []}
 
     for fi in folders:
         ## Get paths and extract base names
@@ -35,8 +36,10 @@ def Object101(directory):
         dOBJ101['baseName'].extend(base_names)
         dOBJ101['label0'].extend(lab)
 
+    dOBJ101['dataset'] = ['101_ObjectCategoreis'] * len(dOBJ101['filePath'])
     #dfOBJ101 = pd.DataFrame(dOBJ101)
     ## Not sure if a dict or a pd.DataFrame should be returned.
+
     return(dOBJ101)
 
 def Cars(directory):
@@ -77,14 +80,39 @@ def Cars(directory):
 
     subset = ['train'] * len(train_labels) + ['test'] * len(test_labels)
 
-    dCars = {'filePath' : paths, 'baseName' : baseNames, 
-             'label0' : labels0, 'labels1' : labels1, 'subset' : subset}
+    dCars = {'dataset' : ['Cars'] * len(paths), 'filePath' : paths, 
+            'baseName' : baseNames, 'label0' : labels0, 
+            'labels1' : labels1, 'subset' : subset}
 
     return(dCars)
 
 def Flowers(directory):
+   
+    flower_dir = directory + '102Flowers/'
     
-    return(0)
+    paths = glob.glob(flower_dir + 'jpg/*.jpg')
+    paths.sort()
+
+    baseNames = [p.split('/')[-1] for p in paths]
+
+    meta = loadmat(flower_dir + 'imagelabels.mat')
+    splits = loadmat(flower_dir + 'setid.mat')
+
+    labels = list(meta['labels'][0])
+
+    name_dict = {}
+
+    with open('102Flower_labels.csv') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            name_dict[int(row[0])] = row[1]
+
+    labels0 = [name_dict[i] for i in labels] 
+
+    dFlowers = {'dataset' : ['102Flowers'] * len(paths), 'filePath' : paths, 
+            'baseName' : baseNames, 'label0' : labels0}
+
+    return(dFlowers)
 
 def Pets(directory):
     pets_dir = directory + "Pets/" 
@@ -128,7 +156,7 @@ def Pets(directory):
 
     subset = ['train'] * len(train_anno) + ['test'] * len(test_anno)
     
-    dPets = {'filePath' : paths, 'baseName' : baseNames, 'label0' :
+    dPets = {'dataset' : ['Pets'] * len(paths), 'filePath' : paths, 'baseName' : baseNames, 'label0' :
             label0, 'lebel1' : label1, 'subset' : subset}
 
     return(dPets)
@@ -157,7 +185,7 @@ def SUN397(directory):
         labels0.extend([lab] * len(tmp))
 
 
-    dSUN = {'filePath' : paths, 'baseName' : baseNames, 'label0' : labels0}
+    dSUN = {'dataset' : ['SUN397'] * len(paths), 'filePath' : paths, 'baseName' : baseNames, 'label0' : labels0}
 
     return(dSUN)
     ## END SUN397
@@ -198,29 +226,114 @@ def dtd(directory):
             if len(row) > 2:
                 labels1.append(row[1])
             else: 
-                labels1.append("NA")
+                labels1.append("NaN")
 
             if len(row) > 3:
                 labels2.append(row[2])
             else: 
-                labels2.append("NA")
+                labels2.append("NaN")
 
             if len(row) > 4:
                 labels3.append(row[3])
             else: 
-                labels3.append("NA")
+                labels3.append("NaN")
 
-    dDTD = {'filePath' : paths, 'baseName' : baseNames, 'label0' :
-            labels0, 'label1' : labels1, 'label2' : labels2, 'label3' :
-            labels3}
+    dDTD = {'dataset' : ['dtd'] * len(paths), 'filePath' : paths, 
+            'baseName' : baseNames, 'label0' : labels0, 
+            'label1' : labels1, 'label2' : labels2, 'label3' : labels3}
 
     return(dDTD)
 
 def fgvc(directory):
-    fgvc_dir = directory + "fgvc-aircraft-2013b/" 
-    classes = os.listdir(fgvc_dir + 'data')
+    fgvc_dir = directory + 'FGVC/fgvc-aircraft-2013b/'
+    data_dir = fgvc_dir + 'data/'
+    image_dir = fgvc_dir + 'data/images/'
 
-    return(0)
+    image_dict = {i.split('.')[0] : {'baseName' : i} for i in os.listdir(image_dir)}
+
+    train_list = ['images_variant_train.txt', 'images_family_train.txt', 'images_manufacturer_train.txt']
+
+    test_list = ['images_variant_test.txt', 'images_family_test.txt', 'images_manufacturer_test.txt']
+
+    with open(data_dir + 'images_variant_train.txt') as f:
+        reader = csv.reader(f, delimiter = ' ')
+        for row in reader:
+            image_dict[row[0]]['variant'] = row[1]
+        
+    with open(data_dir + 'images_variant_test.txt') as f:
+        reader = csv.reader(f, delimiter = ' ')
+        for row in reader:
+            image_dict[row[0]]['variant'] = row[1]
+
+    with open(data_dir + 'images_variant_trainval.txt') as f:
+        reader = csv.reader(f, delimiter = ' ')
+        for row in reader:
+            image_dict[row[0]]['variant'] = row[1]
+        
+    with open(data_dir + 'images_variant_val.txt') as f:
+        reader = csv.reader(f, delimiter = ' ')
+        for row in reader:
+            image_dict[row[0]]['variant'] = row[1]
+        
+    with open(data_dir + 'images_family_train.txt') as f:
+        reader = csv.reader(f, delimiter = ' ')
+        for row in reader:
+            image_dict[row[0]]['family'] = row[1]
+        
+    with open(data_dir + 'images_family_test.txt') as f:
+        reader = csv.reader(f, delimiter = ' ')
+        for row in reader:
+            image_dict[row[0]]['family'] = row[1]
+
+    with open(data_dir + 'images_family_trainval.txt') as f:
+        reader = csv.reader(f, delimiter = ' ')
+        for row in reader:
+            image_dict[row[0]]['family'] = row[1]
+        
+    with open(data_dir + 'images_family_val.txt') as f:
+        reader = csv.reader(f, delimiter = ' ')
+        for row in reader:
+            image_dict[row[0]]['family'] = row[1]
+        
+    with open(data_dir + 'images_manufacturer_train.txt') as f:
+        reader = csv.reader(f, delimiter = ' ')
+        for row in reader:
+            image_dict[row[0]]['manufacturer'] = row[1]
+        
+    with open(data_dir + 'images_manufacturer_test.txt') as f:
+        reader = csv.reader(f, delimiter = ' ')
+        for row in reader:
+            image_dict[row[0]]['manufacturer'] = row[1]
+
+    with open(data_dir + 'images_manufacturer_trainval.txt') as f:
+        reader = csv.reader(f, delimiter = ' ')
+        for row in reader:
+            image_dict[row[0]]['manufacturer'] = row[1]
+        
+    with open(data_dir + 'images_manufacturer_val.txt') as f:
+        reader = csv.reader(f, delimiter = ' ')
+        for row in reader:
+            image_dict[row[0]]['manufacturer'] = row[1]
+
+    paths = []
+    baseNames = []
+    labels0 = []
+    labels1 = []
+    labels2 = []
+
+    for key in image_dict:
+       paths.append(image_dir + image_dict[key]['baseName'])
+       baseNames.append(image_dict[key]['baseName'])
+       labels0.append(image_dict[key]['variant'])
+       labels1.append(image_dict[key]['family'])
+       labels2.append(image_dict[key]['manufacturer'])
+
+
+    dFGVC = {'dataset' : ['fgvc'] * len(paths), 'filePath' : paths, 
+            'baseName' : baseNames, 'label0' : labels0, 
+            'label1' : labels1, 'label2' : labels2}
+
+    return(dFGVC)
 
 def food101(directory):
     food_dir = directory + "food-101/images/" 
@@ -238,7 +351,7 @@ def food101(directory):
         baseNames.extend([b.split('/')[5] for b in tmp])
         labels0.extend([ci] * len(tmp))
         
-    dFood = {'filePath' : paths, 'baseName' : baseNames, 'label0' : labels0}
+    dFood = {'dataset' : ['food101'] * len(paths), 'filePath' : paths, 'baseName' : baseNames, 'label0' : labels0}
 
     return(dFood)
 
@@ -248,19 +361,51 @@ def mergeAll():
     return(0)
 
 def main(directory):
-    obj = Object101(directory)
-    return(0)
+
+    #datasets = {
+    #        'obj101' : Object101(directory),
+    #        'fgvc'   : fgvc(directory),
+    #        'food'   : food101(directory),
+    #        'dtd'    : dtd(directory)
+    #        }
+
+    datasets = [
+            pd.DataFrame(Object101(directory)),
+            pd.DataFrame(fgvc(directory)),
+            pd.DataFrame(food101(directory)),
+            pd.DataFrame(Cars(directory)),
+            pd.DataFrame(SUN397(directory)),
+            pd.DataFrame(dtd(directory))
+            ]
+    
+    return(datasets)
 
 
     
 if __name__ == "__main__":
 
+    wd = os.getcwd()
     directory = "../datasets_resized_wLabels/"
 
-    main(directory)
+    datasets = main(directory)
+
+    A = pd.concat(datasets)
+
+    
+
     
 
 
+    dA = {'dataset' : [1], 'filePath' : ['A'], 'label0' : ['a0']}
+    dB = {'dataset' : [2], 'filePath' : ['B'], 'label0' : ['l0'], 'label1' : ['l1']}
+    dC = {'dataset' : [3], 'filePath' : ['C'], 'label0' : ['ab0'],
+            'label1' : ['ab1'], 'label2' : ['ab2']}
+
+    A = pd.DataFrame(dA)
+    B = pd.DataFrame(dB)
+    C = pd.DataFrame(dC)
+
+pd.concat([A,B,C])
 
 
 
